eedb21c1a4aa38b6511c30c3a77f0d40dbfe4765
KaitlinLarson <larsonk20@up.edu>
2017-12-04 19:03:00 -0800

    smart ai (not working)

--- app/src/main/java/edu/up/cs301/phase10/P10SmartComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/phase10/P10SmartComputerPlayer.java
@@ -10,57 +10,18 @@ import edu.up.cs301.game.actionMsg.GameAction;
 import edu.up.cs301.game.infoMsg.GameInfo;
 
 /**
- * Created by josephsmoyer on 11/8/17.
- *
- * ///P10SmartComputerPlayer:///
- *
- * /DRAW from Discard or Draw pile according to hand and phase:/
- *
- * if phase has not yet been played, check hand.
- * - Phase 1 (SET OF THREE, SET OF THREE):
- * If there are sets of 2 in hand,
- * check discard pile for matching cards.
- * Pull from discard (return FALSE, break).
- * - Phase 2 (SET OF THREE, RUN OF FOUR):
- * If there is a set of two and not a set of three,
- * check discard pile for matching cards.
- * Then, if there is a run of 2-3,
- * check discard pile
- * - Phase 3 (SET OF FOUR, RUN OF FOUR):
- * - Phase 4 (RUN OF SEVEN):
- * - Phase 5 (RUN OF EIGHT):
- * - Phase 6 (RUN OF NINE):
- * - Phase 7 (SET OF FOUR, SET OF FOUR):
- * - Phase 8 (SEVEN OF ONE COLOR):
- * - Phase 9 (SET OF FIVE, SET OF TWO):
- * - Phase 10 (SET OF FIVE, SET OF THREE):
- *
- * if phase has been played OR no matches in hand,
- * check all ready made phases.
- * if discard matches set, run, or color (PHASE 8 only),
- * draw from discard (return FALSE, break).
- *
- * take from draw (TRUE).
- *
- * /DISCARD least important cards:/
- *
- * @author Trenton Langer
- * @version nov 2017
- * base code taken from P10DumbComputerPlayer
- *
- * @author Kaitlin Larson
- * @version Nov 2017
- * draw logic
+ * Created by Trenton on 11/18/2017.
  */
 
 public class P10SmartComputerPlayer extends P10ComputerPlayer {
-
-    public P10SmartComputerPlayer(String playerName) {
+    P10SmartComputerPlayer(String playerName){
         super(playerName);
     }
 
     @Override
-    protected void receiveInfo(GameInfo info) {
+    protected void receiveInfo(GameInfo info){
+        String message = "Computer"+Integer.toString(playerNum);
+        //Log.i(message, "Received");
 
         // if we don't have a game-state, ignore
         if (!(info instanceof P10State)) {
@@ -70,26 +31,21 @@ public class P10SmartComputerPlayer extends P10ComputerPlayer {
         // update our state variable
         savedState = (P10State)info;
 
-        // On player turn:
+        Log.i("Players hand size", Integer.toString(savedState.getHand(playerNum).size()));
+
+        //if its this players turn
         if (savedState.getToPlay() == this.playerNum) {
             String turnIs = Integer.toString(savedState.getToPlay());
-
-            // get phase.
-            int myPhase = savedState.getPhases()[playerNum];
-
-            // get cards
-            Deck myHand = savedState.getHand(playerNum);
-
+            Log.i("Players turn", turnIs);
             // delay for 0.5 seconds
             sleep(500);
 
-            // create a generic action to be set and sent later
+            //create an generic action to be set and sent later
             GameAction myAction = null;
 
             //if the next valid action is a draw
             if(savedState.getShouldDraw()){
-
-                Log.i("Smart Player","drawing");
+                Log.i("Smart Player", "drawing");
 
                 // look at top discard card
                 Card discardCard = savedState.peekDiscardCard();
@@ -97,35 +53,31 @@ public class P10SmartComputerPlayer extends P10ComputerPlayer {
                 // unless the following checks return anything, the ai will pull from draw pile by default
                 boolean chooseDrawPile = true;
 
-                Log.i("Smart Player",""+savedState.getPlayedPhase()[playerNum][0]);
+                Log.i("Smart Player", "" + savedState.getPlayedPhase()[playerNum][0]);
                 // if they have not played a phase
-                if ( savedState.getPlayedPhase()[playerNum][0].peekAt(0) == null ) {
+                if (savedState.getPlayedPhase()[playerNum][0].peekAt(0) == null) {
 
-                    Log.i("Smart Player","no phase played");
+                    Log.i("Smart Player", "no phase played");
 
                     // if phase is already in hand, forget about this
-                    if ( validPhase(myHand, myPhase) != null ) {
+                    if (validPhase(savedState.getHand(playerNum), savedState.getPhases()[playerNum]) != null) {
 
-                        Log.i("Smart Player","phase in hand");
+                        Log.i("Smart Player", "phase in hand");
+
+                        char[] newCheck;
 
                         // check cards with phase
-                        switch (myPhase) {
+                        switch (savedState.getPhases()[playerNum]) {
                             case 1:
-                                // check hand for sets of two
-                                for ( int i = 0; i < myHand.deckSize(); i++ ) {
-                                    // compare each card in hand --
-                                    Card check = myHand.peekAt(i);
-                                    for ( int j = 0; j < myHand.deckSize(); j++ ) {
-                                        // -- to each other card in hand, except itself
-                                        if ( j == i ) {
-                                            break;
-                                        }
-                                        if ( check.getRank() == myHand.peekAt(j).getRank() ) {
-                                            // if they have the same rank, check discard
-                                            if ( check.getRank() == discardCard.getRank() ) {
-                                                chooseDrawPile = false;
-                                                Log.i("Smart Player","discard matches hand");
-                                            }
+                                // check hand for sets
+                                newCheck = possibleSets(3, savedState.getHand(playerNum));
+                                if (newCheck != null) {
+                                    for (int i = 0; i < newCheck.length; i++) {
+                                        Log.i("Smart Player","possible set: "+newCheck[i]);
+                                        Log.i("Smart Player",discardCard.getRank().shortName()+"");
+                                        if (newCheck[i] == discardCard.getRank().shortName()) {
+                                            chooseDrawPile = false;
+                                            Log.i("Smart Player", "draw from discard");
                                         }
                                     }
                                 }
@@ -134,9 +86,19 @@ public class P10SmartComputerPlayer extends P10ComputerPlayer {
                     }
                 }
 
-                myAction = new P10DrawCardAction(this, chooseDrawPile);  //dumb player always draws from draw pile
+                // if they have played a phase or can't find match to their own phase
+                for (int i = 0; i<savedState.getNumberPlayers(); i++) {
+                    if ( savedState.getPlayedPhase()[i][0].size() > 0 ) {
+                        Log.i("Smart Player","Opponent "+i+": "+savedState.getPlayedPhase()[i][0].toString());
+                        if ( discardCard.getRank() == savedState.getPlayedPhase()[i][0].peekAtTopCard().getRank() ) {
+                            chooseDrawPile = false;
+                        }
+                    }
+                }
 
-                Log.i("Smart Player","chooseDrawPile = "+chooseDrawPile);
+                Log.i("Smart Player", "chooseDrawPile = " + chooseDrawPile);
+
+                myAction = new P10DrawCardAction(this, chooseDrawPile);  //dumb player always draws from draw pile
             }
             else { //if its not time to draw
                 //if neither phase component has been made
@@ -283,27 +245,118 @@ public class P10SmartComputerPlayer extends P10ComputerPlayer {
 
 
         return toDiscard;
-
-
     }
+
+
 
     /* possibleSets(), possibleRanks(), possibleColors()
      * finds which sets/ranks/color combos are most likely to be made
      */
 
-    private int[] possibleSets(int setLength, Deck playerDeck) {
+    private char[] possibleSets(int setLength, Deck playerDeck) {
         int[] ranks = { 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0};
-                        // { 1, 2, 3, etc
+                0, 0, 0, 0, 0, 0};
+        // { 1, 2, 3, etc
         if (setLength>1) {
+            int n = 12;
             for(int i = 0;i<playerDeck.size();i++) {
+                char newRank = playerDeck.peekAt(i).getRank().shortName();
+                if( newRank < 10 ) {
+                    ranks[playerDeck.peekAt(i).getRank().shortName()]++;
+                } else if ( newRank == 't' ) {
+                    ranks[9]++;
+                } else if ( newRank == 'e' ) {
+                    ranks[10]++;
+                } else if ( newRank == 'v' ) {
+                    ranks[11]++;
+                } else if ( newRank == 'w' ) {
+                    for (int j = 0;j<ranks.length;j++) {
+                        ranks[j]++;
+                    }
+                } else {
+                    n--;
+                }
+            }
 
+            char[] goodRanks = null;
+            if ( n>0 ) {
+                goodRanks = new char[n];
+            }
+
+            if ( goodRanks != null ) {
+                n = 0;
+                for (int i = 0; i < ranks.length; i++) {
+                    if (ranks[i] > 1 && ranks[i] < setLength) {
+                        if (i<9) {
+                            goodRanks[n] = (char)(i + 1);
+                        } else if (i == 9) {
+                            goodRanks[n] = 't';
+                        } else if (i == 10) {
+                            goodRanks[n] = 'e';
+                        } else if (i == 11) {
+                            goodRanks[n] = 'v';
+                        }
+                        n++;
+                    }
+                }
+                return goodRanks;
             }
         }
         return null;
     }
 
-    private int[][] possibleRanks(int rankLength, Deck playerDeck) {
+    private char[] possibleRuns(int runLength, Deck playerDeck) {
+        int[] ranks = { 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0};
+        // { 1, 2, 3, etc
+        if (runLength>1) {
+            int n = 12;
+            for(int i = 0;i<playerDeck.size();i++) {
+                char newRank = playerDeck.peekAt(i).getRank().shortName();
+                if( newRank < 10 ) {
+                    ranks[playerDeck.peekAt(i).getRank().shortName()]++;
+                } else if ( newRank == 't' ) {
+                    ranks[9]++;
+                } else if ( newRank == 'e' ) {
+                    ranks[10]++;
+                } else if ( newRank == 'v' ) {
+                    ranks[11]++;
+                } else if ( newRank == 'w' ) {
+                    for (int j = 0;j<ranks.length;j++) {
+                        ranks[j]++;
+                    }
+                } else {
+                    n--;
+                }
+            }
+
+            int[] allRanks = null;
+            if ( n>0 ) {
+                allRanks = new int[n];
+            }
+
+            if ( allRanks != null ) {
+                n = 0;
+                for (int i = 0; i < ranks.length; i++) {
+                    if (ranks[i] > 1) {
+                        if (i<9) {
+                            allRanks[n] = i + 1;
+                        } else if (i == 9) {
+                            allRanks[n] = 10;
+                        } else if (i == 10) {
+                            allRanks[n] = 11;
+                        } else if (i == 11) {
+                            allRanks[n] = 12;
+                        }
+                        n++;
+                    }
+                }
+
+                for (int i = 0;i<runLength;i++) {
+
+                }
+            }
+        }
         return null;
     }
 
